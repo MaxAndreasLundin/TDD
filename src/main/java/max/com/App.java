9f4df2e62206bbb3d83fe9b1c3a74b966c26ab6c
MaxAndreasLundin <lundin-89@hotmail.com>
2022-03-25 14:00:45 +0100

    SeleniumTest finished.

--- src/main/java/max/com/App.java
+++ src/main/java/max/com/App.java
@@ -4,10 +4,11 @@ package max.com;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!!!" );
+        System.out.println( "Hello World!" );
     }
 }
+

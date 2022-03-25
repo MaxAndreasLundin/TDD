712e1e35a1d6ac274427979f43ef6cf8324f93a5
MaxAndreasLundin <lundin-89@hotmail.com>
2022-03-25 20:26:24 +0100

    SeleniumFirefoxTest Refactored.

--- src/test/java/max/com/SeleniumFirefoxTest.java
+++ src/test/java/max/com/SeleniumFirefoxTest.java
@@ -4,7 +4,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
@@ -13,20 +15,17 @@ import static org.junit.Assert.fail;
 
 public class SeleniumFirefoxTest {
     private WebDriver driver;
-    private String baseUrl;
-    private boolean acceptNextAlert = true;
     private final StringBuffer verificationErrors = new StringBuffer();
 
     @Before
     public void setUp() {
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();
-        baseUrl = "https://www.google.com/";
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     }
 
     @Test
-    public void testUntitledTestCase() {
+    public void webPageTest() {
         driver.get("https://www.sweclockers.com/");
         driver.findElement(By.xpath("//input[@type='text']")).click();
         driver.findElement(By.xpath("//input[@type='text']")).clear();
@@ -41,39 +40,6 @@ public class SeleniumFirefoxTest {
         String verificationErrorString = verificationErrors.toString();
         if (!"".equals(verificationErrorString)) {
             fail(verificationErrorString);
-        }
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
         }
     }
 }

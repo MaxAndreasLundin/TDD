4ecb6781f5454ab29b1d4ff6443cb646596cc056
MaxAndreasLundin <lundin-89@hotmail.com>
2022-03-31 11:48:58 +0200

    Added SeleniumFirefoxTest

---
+++ src/test/java/max/com/SeleniumFirefoxTest.java
@@ -0,0 +1,79 @@
+package max.com;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+import static org.junit.Assert.fail;
+
+public class SeleniumFirefoxTest {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        baseUrl = "https://www.sweclockers.com/";
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+
+    @Test
+    public void testUntitledTestCase() {
+        driver.get("https://www.sweclockers.com/");
+        driver.findElement(By.xpath("//input[@type='text']")).click();
+        driver.findElement(By.xpath("//input[@type='text']")).clear();
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("rtx 3090");
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys(Keys.ENTER);
+        driver.findElement(By.linkText("Apple M1 Ultra utpresterar inte Nvidia Geforce RTX 3090")).click();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
